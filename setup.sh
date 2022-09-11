ca8d8e4be0e8d24ed559cfa998a12855a95a02e0
Stephen Welch <stephenwelch@vt.edu>
2022-09-11 05:13:31 -0400

    Pull pandora submodules in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ vcs import < src/ros2.repos src
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
+
+# pull submodules
+cd /workspaces/pandora-workspace/src/pandora
+git submodule update --init --recursive
